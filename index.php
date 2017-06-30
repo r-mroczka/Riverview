9865b640ce62971273f30e2655b53ad8d5bf11c3
Robert Mroczka <r.mroczka@meamobile.com>
2017-06-29 23:12:29 -0400

    Home page almost complete

--- index.php
+++ index.php
@@ -5,48 +5,48 @@ include_once("./includes/header.php")
 	
 
 <section class="home_page">
-  <div class="jumbotron text-center">
-    <div class="container">
-      <div class="row" style="height: 350px;">
-        <div class="col-xs12">
-        	<!-- place content here -->
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="jumbotron text-center">
+		<div class="container">
+			<div class="row" style="height: 350px;">
+				<div class="col-xs12">
+					<h2 style=" color: black;">Sober Housing - New Haven, CT</h2>
+					<p style="color: white; text-shadow: 1px 1px 3px #000;">For nearly 10 years, we have been positively transforming the lives of our residents.
+					<br>We take a comprehensive approach to addiction treatment and sober living that<br>is designed to gradually reintegrate our clients back into the real world.</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
 
 <section>
-  <div class="container well" style="background-color: black; color: #7c7b7b; border: none;">
-   	<h2 style="text-align: center; margin-bottom: 25px;">Sober Atmosphere that Supports Independent Living</h2>
+	<div class="container well" style="background-color: black; color: #7c7b7b; border: none;">
+	<h2 style="text-align: center; margin-bottom: 25px;">Sober Atmosphere That Supports Independent Living</h2>
 
-    <div class="row">
-      <div class="col-lg-4 col-md-4 col-sm-4">
-        <div class="media">
-          <div class="media-left"> <a href="#"> <img class="media-object" src="./images/35X35.gif" width="35px" height="35px" alt="..."> </a> </div>
-          <div class="media-body">
-            <h3 class="media-heading text-center">Quality</h3>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. </div>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-4 col-sm-4">
-        <div class="media">
-          <div class="media-left"> <a href="#"> <img class="media-object" src="./images/35X35.gif" width="35px" height="35px" alt="..."> </a> </div>
-          <div class="media-body">
-            <h3 class="media-heading text-center">Sober</h3>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. </div>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-4 col-sm-4">
-        <div class="media">
-          <div class="media-left"> <a href="#"> <img class="media-object" src="./images/35X35.gif" width="35px" height="35px" alt="..."></a></div>
-          <div class="media-body">
-            <h3 class="media-heading text-center">Housing</h3>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. </div>
-        </div>
-      </div>
-    </div>
-  </div>
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-sm-4 text-center">
+				<div style="width: 80%; margin: 0 auto;">
+					<a href="#"> <img style="margin-bottom: 10px;" src="./images/house_1.jpg" width="100%" alt="..."> </a> 
+					<h3>Quality</h3>
+					<p>Located in a quiet, historic part of New Haven away from the hustle and bustle yet steps away from main bus lines and minutes from down town.</p>
+				</div> 
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 text-center">
+				<div style="width: 80%; margin: 0 auto;">
+					<a href="#"> <img style="margin-bottom: 10px;" src="./images/house_2.jpg" width="100%" alt="..."> </a> 
+					<h3>Sober</h3>
+					<p>Sober living homes provide support to people in early recovery to help them rebuild their lives. Residents moving into the houses are encouraged to have a treatment plan for their recovery and are held accountable to follow through on their plans. </p>
+				</div> 
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 text-center">
+				<div style="width: 80%; margin: 0 auto;">
+					<a href="#"> <img style="margin-bottom: 10px;" src="./images/house_3.jpg" width="100%" alt="..."> </a> 
+					<h3>Housing</h3>
+					<p>Beautiful, newly renovated historic home originally built in the 1850's overlooking the Quinnipiac River. We offer spacious, comfortable and home like living environment that is very supportive and with a great atmosphere. </p>
+				</div> 
+			</div>
+		</div>
+
+	</div>
 </section>
 
 <hr>
@@ -54,8 +54,8 @@ include_once("./includes/header.php")
   <div class="container">
     <div class="row">
       <div class="col-lg-12 text-center">
-        <h1>Lorem ipsum dolor sit amet</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, magni, doloribus, possimus eum sapiente deleniti doloremque fugit ut expedita molestiae iusto debitis eveniet modi obcaecati ipsam quos quis labore dicta.</p>
+        <h2>Addmision</h2>
+        <p>Riverview Sober House is a professionally run facility with a full time, live-in manager and assistant manager. We have zero tolerance substance use policy. routine but random drug and alcohol testing, a requirement of attending at least 3 AA (Alcoholic Anonymous) or NA (Narcotic Anonymous) meetings per week and generally a minimum of 4 months sobriety required for admission. All residents are required to have or be actively full time employment and/or education. Additionally all residents are subject to house rules designed to enhance and encourage sober,  independent living. </p>
 		<button type="button" class="btn btn-success">Get in touch</button>
       </div>
     </div>
@@ -66,20 +66,21 @@ include_once("./includes/header.php")
     <div class="container">
    	  <div class="row">
 		<div class="col-lg-4 col-md-4">
-            <h3 class="text-center">WHO WE ARE</h3>
-            <h5 style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, ducimus, sit, quibusdam quidem recusandae veniam eos quod error nisi repellat excepturi laboriosam aspernatur suscipit possimus consectetur dolores nihil labore quas eius illo accusamus nulla sed blanditiis porro accusantium. Perspiciatis, perferendis!</h5>
+			<!-- content can go here -->
         </div>
+		
+	  	<div class="col-lg-4 col-md-4">
+		  <h4 class="text-center">GET IN TOUCH</h4>
+          <address class="text-center">
+		  <strong>Riverview Sober House</strong><br>
+		  New Haven<br>
+		  CT, 06510<br>
+		  860-456-7890
+		</address>
+		</div>
+		
 		<div class="col-lg-4 col-md-4">
 			<!-- content can go here -->
-		</div>
-		<div class="col-lg-4 col-md-4">
-		  <h3 class="text-center">GET IN TOUCH</h3>
-          <address class="text-center">
-		  <strong>MyCompany, Inc.</strong><br>
-		  Sunny Autumn Plaza, Grand Coulee,<br>
-		  CA, 91308-4075, US<br>
-		  <abbr title="Phone">P:</abbr> (123) 456-7890
-		</address>
 		</div>
 	  </div>
     </div>
